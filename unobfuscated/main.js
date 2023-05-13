a46ae3363aa8c3d37bd45f9747c6b5991a35dd13
xShadowBlade <111022502+xShadowBlade@users.noreply.github.com>
2023-05-12 22:11:15 -0400

    output outline

--- unobfuscated/main.js
+++ unobfuscated/main.js
@@ -1,19 +1,22 @@
 (async function () {
 const core = {
+    html: {},
     functions: {
         init: {
             container: () => {
                 // Create a new div element for the white rectangle
-                const container = document.createElement("div");
+                core.html.container = document.createElement("div");
 
+                const container = core.html.container;
                 // Set the CSS styles for the white rectangle
                 container.style.position = "fixed";
                 container.style.top = "0";
                 container.style.right = "0";
-                container.style.width = "20%";
+                container.style.width = "30%";
                 container.style.height = "100%";
                 container.style.background = "white";
                 container.style.zIndex = "9999";
+                container.style.border = "3px solid";
 
                 // Add the div element to the document
                 document.body.appendChild(container);
@@ -23,6 +26,28 @@ const core = {
                 document.body.style.transformOrigin = "50% 20%";
                 // document.body.style.transform = "scale(1.5) translate(50px, 50px)";
                 // document.body.style.transformOrigin = "top left";
+            },
+            console: () => {
+                core.html.cconsole = {};
+
+                // Create output element
+                core.html.cconsole.consoleOut = document.createElement("div");
+
+                const cconsoleOut = core.html.cconsole.consoleOut;
+                // Set the CSS styles for the white rectangle
+                cconsoleOut.style.position = "fixed";
+                cconsoleOut.style.top = "0";
+                cconsoleOut.style.right = "0";
+                cconsoleOut.style.width = "30%";
+                cconsoleOut.style.height = "70%";
+                cconsoleOut.style.background = "white";
+                cconsoleOut.style.zIndex = "9999";
+                cconsoleOut.style.border = "1px solid";
+
+                // Add the div element to the document
+                document.body.appendChild(cconsoleOut);
+
+                cconsoleOut.innerHTML = "test";
             }
         },
         loop: {}
